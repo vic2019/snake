89d44dbad804d62f9478556efbaf3d7377314994
Victor <victor.fan.wang@gmail.com>
2021-05-27 19:20:43 -0700

    Delete unused files

--- getkey.js
+++
@@ -1,7 +0,0 @@
-const readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', (_, key) => {
-  if (key.ctrl && key.name === 'c') return process.exit();
-  console.log(key);
-});
